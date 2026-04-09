13fadcc402ad88a864cee1a1344ccb3610e7e5f5
priteshsarva <priteshsarva9825@gmail.com>
2026-04-09 11:33:11 +0530

    DB updated on 2026-04-09 06:03:10

--- core/strategies/methodB.js
+++ core/strategies/methodB.js
@@ -400,12 +400,14 @@ async function scrapeProducts(page, categories, baseUrl, DB) {
             for (const eachproduct of catProductss) {
                 updateProductCategory(eachproduct);
                 const { productId, skipforwordpress } = await updateProduct(eachproduct, DB);
-
+                //for temporary disable
+                await syncProductToAllSites(eachproduct, productId);
                 if (skipforwordpress) {
                     console.log("Skipped upsertProductSafe due to WordPress flag. ProductID = " + productId);
                 } else {
                     console.log("upsertProductSafe with id" + productId)
-                    await syncProductToAllSites(eachproduct, productId);
+                    //for temporary 
+                    // await syncProductToAllSites(eachproduct, productId);
                 }
                 console.log("From Each Product");
             }
