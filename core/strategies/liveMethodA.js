badff796400e3e01f4040075a81c318d42519519
priteshsarva <priteshsarva9825@gmail.com>
2026-04-27 23:42:44 +0530

    DB updated on 2026-04-27 18:12:44

--- core/strategies/liveMethodA.js
+++ core/strategies/liveMethodA.js
@@ -18,7 +18,7 @@ export async function scrapeSingleProductMethodA(productUrl, dbName) {
 
     try {
         const browser = await puppeteer.launch({
-            headless: false,
+            headless: true,
             executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath(),
             // Make viewport smaller so Chrome uses less RAM to render the page
             defaultViewport: { width: 800, height: 600 },
